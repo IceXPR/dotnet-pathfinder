f5b2bc4923adc107a7ba0fc081925fad221f9c32
Julio <IceXPR@users.noreply.github.com>
2022-08-25 02:28:20 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Pathfinder;
+
+public class Pathfinder
+{
+
+  public int findPaths(int[,] street, int start_i, int start_j, int acc)
+
+  {
+
+    for (int i = start_i; i < street.GetLength(0); i++) // iterate horizontal
+    { 
+      for (int j = start_j; j < street.GetLength(1); j++)  // iterate vertical
+      {
+        if (i == 2 && j == 2)
+        {
+          return acc + 1;  // reach exit block
+        }
+        else if (street[i, j] == 0 || i + 1 >= street.GetLength(0)) //TODO: not sure i+1
+        {
+          return acc;
+        }
+        else
+        {
+          acc = findPaths(street, i + 1, j, acc); //move down
+        }
+      }
+    }
+    return acc;
+  }
+  public static void Main()
+  {
+    int[,] map0 = new int[3, 3] { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+    int[,] map1 = new int[3, 3] { { 1, 1, 1 }, { 0, 1, 1 }, { 1, 1, 1 } };
+    Pathfinder p = new Pathfinder();
+    Console.Out.WriteLine("number of paths on map0: " + p.findPaths(map0, 0, 0, 0));
+    Console.Out.WriteLine("number of paths on map1: " + p.findPaths(map1, 0, 0, 0));
+  }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# DOTNET-PATHFINDER
+
+An C# sample function to find the route given an array providing the passage where 0 is blocked and 1 is drivible.  The route starts at 0,0 and ends on 2,2.
+
+## Examples
+```
+map0 = [
+  [1,1,1]
+  [1,0,1]
+  [1,1,1]
+  ] = 2 paths
+
+map1 = [
+  [1,1,1]
+  [0,1,1]
+  [1,1,1]
+] = 3 paths
+```
+
+## Execution
+```bash
+dotnet run
+```
+
+## REFERENCES
+- https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/multidimensional-arrays
+- https://stackoverflow.com/questions/9404683/how-to-get-the-length-of-row-column-of-multidimensional-array-in-c
